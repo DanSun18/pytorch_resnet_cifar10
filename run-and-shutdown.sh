4192135ef34c9ee5245523d66b6f64556a7bf5e1
Dan Sun <dansun47@gmail.com>
2020-03-22 03:37:40 +0000

    added script for shutting down after training; for EC2 use

--- run-and-shutdown.sh
+++ run-and-shutdown.sh
@@ -3,4 +3,8 @@
 # to be used on EC2 instances to shutdown machine after training completes.
 . run.sh
 sleep 5
-sudo shutdown -h now
+sudo shutdown -h now # -h needs to be included or EC2 reports running with error
+                     # make sure shutdown behavior is set correctly (stop vs. terminate)
+                     # also enable termination protection if that helps
+                     # if you cannot it might mean the instance can only terminate (it cannot be stopped).
+                     # Examples include non-persistent spot instances.
