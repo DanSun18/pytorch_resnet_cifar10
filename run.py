9b7b91be708d962fd2dda30d07a49f8af1aa96da
Dan Sun <dansun47@gmail.com>
2020-03-26 22:07:33 +0000

    added command line parser for models to train and shutdown behavior

--- run.py
+++ run.py
@@ -2,10 +2,26 @@
 # incorporated shutdown capability as well, from run-and-shutdown.sh
 
 import os
+import argparse 
 
-shutdown = False #boolean indicating whether to shutdown machine after program execution
+parser = argparse.ArgumentParser(description='Runs training for specified ResNet architectures')
+parser.add_argument('models', help='models to be trained', 
+                              nargs='+',
+                              metavar = 'resnetXX',
+                              choices=['all', 'resnet20', 'resnet32', 'resnet44', 'resnet56' ,'resnet110' ,'resnet1202'])  
+#nargs specify how many arguments is accepted for this particular option. '+' means all and generate error message when there is none.
+#metavar just alters the displayed help message for better clarification.
+#resnet1202 is only provided as a potential option. Wouldn't use it generally.
+parser.add_argument('--shutdown', help='shutdown machine after execution',
+                    action='store_true')
 
-if shutdown:
+args = parser.parse_args()
+print(args.models)
+print(args.shutdown)
+
+models_from_all = ['resnet20', 'resnet32', 'resnet44', 'resnet56' ,'resnet110'] # does not include resnet1202 for cost concerns and training time.
+
+if args.shutdown:
     cmd = 'sudo shutdown -h now'
     os.system(cmd)
     
