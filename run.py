0bfcd3e4fff80d42b2de25caad85bb3393eca49e
Dan Sun <dansun47@gmail.com>
2020-03-27 02:03:34 +0000

    finished run.py

--- run.py
+++ run.py
@@ -4,25 +4,51 @@
 import os
 import argparse 
 
-parser = argparse.ArgumentParser(description='Runs training for specified ResNet architectures')
-parser.add_argument('models', help='models to be trained', 
+def createArgParser():
+    
+    parser = argparse.ArgumentParser(description='Runs training for specified ResNet architectures')
+    parser.add_argument('models', help='models to be trained', 
                               nargs='+',
                               metavar = 'resnetXX',
                               choices=['all', 'resnet20', 'resnet32', 'resnet44', 'resnet56' ,'resnet110' ,'resnet1202'])  
-#nargs specify how many arguments is accepted for this particular option. '+' means all and generate error message when there is none.
-#metavar just alters the displayed help message for better clarification.
-#resnet1202 is only provided as a potential option. Wouldn't use it generally.
-parser.add_argument('--shutdown', help='shutdown machine after execution',
-                    action='store_true')
+    #nargs specify how many arguments is accepted for this particular option. '+' means all and generate error message when there is none.
+    #metavar just alters the displayed help message for better clarification.
+    #resnet1202 is only provided as a potential option. Wouldn't use it generally.
+    parser.add_argument('--shutdown', help='shutdown machine after execution',
+                        action='store_true')
+    return parser
 
+def runTrainer(models):
+    for model in models:
+        cmd = 'python -u trainer.py'
+        cmd = cmd + ' --arch=' + model 
+        cmd = cmd + ' --save-dir=save_' + model 
+        cmd = cmd + ' |& tee -a ' + model + '.log'
+    print('Running trainer for ' + model + ':\n', cmd)
+    os.system(cmd)
+    return None
+
+parser = createArgParser()
 args = parser.parse_args()
-print(args.models)
-print(args.shutdown)
 
+# expand all into actual model names
 models_from_all = ['resnet20', 'resnet32', 'resnet44', 'resnet56' ,'resnet110'] # does not include resnet1202 for cost concerns and training time.
+if 'all' in args.models:
+    args.models = models_from_all
 
+# logging information
+print("The program will run trainer for {}".format(', '.join(args.models)))
 if args.shutdown:
-    cmd = 'sudo shutdown -h now'
+    print("IMPORTANT: Machine will be shutdown after program finishes execution")
+
+runTrainer(args.models)
+
+#shutdown if specified in args
+if args.shutdown:
+    shutdown_timer = '5s'
+    print("Shutting down in " + shutdown_timer)
+    cmd = 'sleep ' + shutdown_timer + ';'
+    cmd = cmd + 'sudo shutdown -h now'
     os.system(cmd)
     
 
