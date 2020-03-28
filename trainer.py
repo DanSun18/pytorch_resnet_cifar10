22e6ed4fee046179be2705ddd1079a5ba74bf52a
Dan Sun <dansun47@gmail.com>
2020-03-28 21:46:45 +0000

    more modificaitons to trainer.py

--- trainer.py
+++ trainer.py
@@ -44,7 +44,7 @@ parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=50, type=int,
-                    metavar='N', help='print frequency (default: 50)')
+                    metavar='N', help='print frequency in number of batches (default: 50)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
@@ -59,7 +59,7 @@ parser.add_argument('--save-dir', dest='save_dir',
 parser.add_argument('--save-every', dest='save_every',
                     help='Saves checkpoints at every specified number of epochs',
                     type=int, default=10)
-best_prec1 = 0 #best precision @ 1 (top 1 -error?)
+best_prec1 = 0 #best precision @ 1 (top-1 error)
 
 
 def main():
@@ -125,7 +125,6 @@ def main():
         criterion.half()
         
     #define learning rate schduler
-
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                                         milestones=[100, 150], last_epoch=args.start_epoch - 1)
 
@@ -143,7 +142,7 @@ def main():
     for epoch in range(args.start_epoch, args.epochs):
 
         # train for one epoch
-        print('current lr {:.5e}'.format(optimizer.param_groups[0]['lr']))
+        print('current lr {:.5e}'.format(optimizer.param_groups[0]['lr'])) #prints learning rate at the start of the epoch
         train(train_loader, model, criterion, optimizer, epoch)
         lr_scheduler.step()
 
@@ -159,7 +158,7 @@ def main():
                 'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
                 'best_prec1': best_prec1,
-            }, is_best, filename=os.path.join(args.save_dir, 'checkpoint.th'))
+            }, is_best, filename=os.path.join(args.save_dir, 'checkpoint.th')) #checkpoint overwrites previous checkpoints
 
         save_checkpoint({
             'state_dict': model.state_dict(),
@@ -172,26 +171,29 @@ def train(train_loader, model, criterion, optimizer, epoch):
         Run one train epoch
     """
     batch_time = AverageMeter()
-    data_time = AverageMeter()
+    data_time = AverageMeter() #data loading time of each batch. Not that important, I think
     losses = AverageMeter()
     top1 = AverageMeter()
 
-    # switch to train mode
+    # switch to training mode
     model.train()
 
     end = time.time()
-    for i, (input, target) in enumerate(train_loader):
+    
+    for i, (input, target) in enumerate(train_loader): # i is the batch-index
 
-        # measure data loading time
+        # measure data loading time (from the train_loader?)
         data_time.update(time.time() - end)
-
+    
+        # put target and input on GPU 
         target = target.cuda()
         input_var = input.cuda()
         target_var = target
-        if args.half:
+        
+        if args.half: #half precision if specified
             input_var = input_var.half()
 
-        # compute output
+        # compute output and calculate loss
         output = model(input_var)
         loss = criterion(output, target_var)
 
@@ -199,18 +201,22 @@ def train(train_loader, model, criterion, optimizer, epoch):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+    
+        #convert output and loss to float (Why?)
         output = output.float()
         loss = loss.float()
-        # measure accuracy and record loss
-        prec1 = accuracy(output.data, target)[0]
+        
+        # calculate accuracy
+        prec1 = accuracy(output.data, target)[0] #note that this accuracy is the accuracy on the current batch.
+        
+        # record/update loss and accuracy for the current batch
         losses.update(loss.item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
 
-        # measure elapsed time
+        # record elapsed time for this batch
         batch_time.update(time.time() - end)
-        end = time.time()
-
+        
+        #print training information as specified
         if i % args.print_freq == 0:
             print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
@@ -219,11 +225,14 @@ def train(train_loader, model, criterion, optimizer, epoch):
                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
                       epoch, i, len(train_loader), batch_time=batch_time,
                       data_time=data_time, loss=losses, top1=top1))
+        
+        end = time.time() #update end time of current batch
 
 
 def validate(val_loader, model, criterion):
     """
     Run evaluation
+    Returns average accuracy on the entire dataset used for validation.
     """
     batch_time = AverageMeter()
     losses = AverageMeter()
@@ -249,14 +258,14 @@ def validate(val_loader, model, criterion):
             output = output.float()
             loss = loss.float()
 
-            # measure accuracy and record loss
+            # measure accuracy and update loss and accuracy
             prec1 = accuracy(output.data, target)[0]
             losses.update(loss.item(), input.size(0))
             top1.update(prec1.item(), input.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
-            end = time.time()
+            
 
             if i % args.print_freq == 0:
                 print('Test: [{0}/{1}]\t'
@@ -265,6 +274,8 @@ def validate(val_loader, model, criterion):
                       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
                           i, len(val_loader), batch_time=batch_time, loss=losses,
                           top1=top1))
+            
+            end = time.time()
 
     print(' * Prec@1 {top1.avg:.3f}'
           .format(top1=top1))
@@ -275,6 +286,7 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     """
     Save the training model
     """
+    # the parameter is_best does not seem to be used?
     torch.save(state, filename)
 
 class AverageMeter(object):
@@ -295,12 +307,13 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
+def accuracy(output, target, topk=(1,)): #one element tuple requires a comma, hence the default value of topk
     """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
+    """ In other words, it returns the top k accuracy"""
+    maxk = max(topk) #really just converts it to an int, I suppose
     batch_size = target.size(0)
 
-    _, pred = output.topk(maxk, 1, True, True)
+    _, pred = output.topk(maxk, 1, True, True) #note that for each datum, the output is actually 10 float values.
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
 
