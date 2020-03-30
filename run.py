61cbd088e7e13a71e7ea664d33979681b81b6a5c
Dan Sun <dansun47@gmail.com>
2020-03-30 01:11:15 +0000

    added shorthand for shutdown for run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def createArgParser():
     #nargs specify how many arguments is accepted for this particular option. '+' means all and generate error message when there is none.
     #metavar just alters the displayed help message for better clarification.
     #resnet1202 is only provided as a potential option. Wouldn't use it generally.
-    parser.add_argument('--shutdown', help='shutdown machine after execution',
+    parser.add_argument('-s', '--shutdown', help='shutdown machine after execution',
                         action='store_true')
     return parser
 
