d424fb2722c264ccaf3f61bad6c67690f2799f17
Dan Sun <dansun47@gmail.com>
2020-04-03 20:01:51 +0000

    added script to archive experimental results

---
+++ archive_results.sh
@@ -0,0 +1,14 @@
+# accepts experiment id as argument
+# moves current experimental results to folder run_* under experiments folder
+
+exp_id=$1
+archive_dir="experiments/run_${exp_id}"
+
+echo "Moving data to ${archive_dir} in 3 seconds"
+sleep 3
+
+mkdir ${archive_dir}
+
+mv resnet*.log ${archive_dir}/.
+mv save_resnet* ${archive_dir}/.
+mv nohup.out ${archive_dir}/.
